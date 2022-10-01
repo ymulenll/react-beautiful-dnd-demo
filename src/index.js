e2ca1b5bf3ff9f212238a3aa866ea361889ef7d6
Yoelvis Mulen <ymulenll@gmail.com>
2022-10-01 14:37:58 -0400

    Upgrade to React 18 migrate to @hello-pangea/dnd

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App";
 
-ReactDOM.render(
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
